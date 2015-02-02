260f0e3a8c22bcc29966f61e6c3e8e21e535b91c
Ashish Kumar <ashish009@live.com>
2015-02-03 02:34:59 +0530

    third commit
    constants file added

---
+++ SecMasterVersionTwo/Secm_Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecMasterVersionTwo
+{
+    class Secm_Constants
+    {
+        public const string i = "insert";
+        public const string d = "delete";
+        public const string u = "update";
+    }
+}
